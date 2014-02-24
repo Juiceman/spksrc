07088b291b3221d4a0402bfaa294883e0be94277
cytec <iamcytec@googlemail.com>
2014-02-24 12:11:40 +0100

    install webogram as webservice

--- spk/webogram/src/dsm-control.sh
+++ spk/webogram/src/dsm-control.sh
@@ -1,77 +1,17 @@
 #!/bin/sh
 
-# Package
-PACKAGE="webogram"
-DNAME="webogram"
-
-# Others
-INSTALL_DIR="/usr/local/${PACKAGE}"
-NODE_DIR="/usr/local/node"
-PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${NODE_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
-USER="root"
-NODE="${NODE_DIR}/bin/node"
-WEBOGRAM="${INSTALL_DIR}/share/webogram/server.js"
-
-start_daemon ()
-{
-    su - ${USER} -c "PATH=${PATH} cd ${INSTALL_DIR}/share/${PACKAGE} &&  ${INSTALL_DIR}/bin/screen -dmS webogram ${NODE} ${WEBOGRAM}"
-}
-
-stop_daemon ()
-{
-    kill `ps | grep "${WEBOGRAM}" | grep -v grep | cut -c1-6`
-    wait_for_status 1 20 || kill -9 `ps | grep "${WEBOGRAM}" | grep -v grep | cut -c1-6`
-}
-
-daemon_status ()
-{
-    if  kill -0 `ps | grep "${WEBOGRAM}" | grep -v grep | cut -c1-6` > /dev/null 2>&1; then
-        return
-    fi
-    return 1
-}
-
-wait_for_status ()
-{
-    counter=$2
-    while [ ${counter} -gt 0 ]; do
-        daemon_status
-        [ $? -eq $1 ] && return
-        let counter=counter-1
-        sleep 1
-    done
-    return 1
-}
-
-
 case $1 in
     start)
-        if daemon_status; then
-            echo ${DNAME} is already running
-        else
-            echo Starting ${DNAME} ...
-            start_daemon
-        fi
+        exit 0
         ;;
     stop)
-        if daemon_status; then
-            echo Stopping ${DNAME} ...
-            stop_daemon
-        else
-            echo ${DNAME} is not running
-        fi
+        exit 0
         ;;
     status)
-        if daemon_status; then
-            echo ${DNAME} is running
-            exit 0
-        else
-            echo ${DNAME} is not running
-            exit 1
-        fi
+        exit 0
         ;;
     log)
-        echo ${LOG_FILE}
+        exit 1
         ;;
     *)
         exit 1
--- spk/webogram/src/installer.sh
+++ spk/webogram/src/installer.sh
@@ -6,14 +6,7 @@ DNAME="webogram"
 
 # Others
 INSTALL_DIR="/usr/local/${PACKAGE}"
-SSS="/var/packages/${PACKAGE}/scripts/start-stop-status"
-NODE_DIR="/usr/local/node"
-PATH="${INSTALL_DIR}/bin:${INSTALL_DIR}/env/bin:${NODE_DIR}/bin:/usr/local/bin:/bin:/usr/bin:/usr/syno/bin"
-NODE="${NODE_DIR}/bin/node"
-NPM="${NODE_DIR}/bin/npm"
-USER="webogram"
-GROUP="users"
-TMP_DIR="${SYNOPKG_PKGDEST}/../../@tmp"
+WEB_DIR="/var/services/web"
 
 
 preinst ()
@@ -26,26 +19,14 @@ postinst ()
     # Link
     ln -s ${SYNOPKG_PKGDEST} ${INSTALL_DIR}
 
-    # Create user
-    adduser -h ${INSTALL_DIR}/var -g "${DNAME} User" -G ${GROUP} -s /bin/sh -S -D ${USER}
-
-    # Correct the files ownership
-    chown -R ${USER}:root ${SYNOPKG_PKGDEST}
+    # Install the web interface
+    cp -R ${INSTALL_DIR}/share/${PACKAGE} ${WEB_DIR}
 
     exit 0
 }
 
 preuninst ()
 {
-    # Stop the package
-    ${SSS} stop > /dev/null
-
-    # Remove the user (if not upgrading)
-    if [ "${SYNOPKG_PKG_STATUS}" != "UPGRADE" ]; then
-        delgroup ${USER} ${GROUP}
-        deluser ${USER}
-    fi
-
     exit 0
 }
 
@@ -54,28 +35,18 @@ postuninst ()
     # Remove link
     rm -f ${INSTALL_DIR}
 
+    # Remove the web interface
+    rm -fr ${WEB_DIR}/${PACKAGE}
+
     exit 0
 }
 
 preupgrade ()
 {
-    # Stop the package
-    ${SSS} stop > /dev/null
-
-    # Save some stuff
-    rm -fr ${TMP_DIR}/${PACKAGE}
-    mkdir -p ${TMP_DIR}/${PACKAGE}
-    mv ${INSTALL_DIR}/var ${TMP_DIR}/${PACKAGE}/
-
     exit 0
 }
 
 postupgrade ()
 {
-    # Restore some stuff
-    rm -fr ${INSTALL_DIR}/var
-    mv ${TMP_DIR}/${PACKAGE}/var ${INSTALL_DIR}/
-    rm -fr ${TMP_DIR}/${PACKAGE}
-
     exit 0
 }
